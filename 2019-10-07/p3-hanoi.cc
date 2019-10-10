63aadfc07d2f8f9536d1286ef8767050b233b7fa
Jordi Petit <jpetit@cs.upc.edu>
2019-10-10 14:38:10 +0200

    Recursivitat 1

--- 2019-10-07/p3-hanoi.cc
+++ 2019-10-07/p3-hanoi.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 // Mostra els moviments per moure n discos del piu orígen al piu destí, passant pel piu auxiliar.
 // Precondició: n >= 0, els dicos a tots els pius estan ben posats i tots els discos de dst i aux
-// són més petits que tots els discos de ori.
+// són més grans que tots els discos de ori.
 void hanoi(int n, int ori, int dst, int aux) {
     if (n > 0) {
         hanoi(n - 1, ori, aux, dst);
